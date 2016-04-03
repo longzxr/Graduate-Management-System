472732587d3132ead640e93e55fbebf01b2543b0
longzxr <longzxr@gmail.com>
2016-04-04 01:15:26 +0800

    update test_oracle.rb

--- test_oracle.rb
+++ test_oracle.rb
@@ -3,8 +3,8 @@
 require 'oci8'
 
 
-sqlcmd = 'select * from xsjbxxb'
+sqlcmd2 = 'select * from xsjbxxb'
 
-OCI8.new('zfxfzb','zfsoft_tzzy','192.168.100.1/orcl').exec(sqlcmd) do |r| puts r.join(','); end
+OCI8.new('zfxfzb','zfsoft_tzzy','192.168.100.1/orcl').exec(sqlcmd2) do |r| puts r.join(','); end
 
 #ruby -r oci8 -e "OCI8.new('zfxfzb', 'zfsoft_tzzy', '192.168.100.1/orcl').exec('select * from xsjbxxb') do |r| puts r.join(','); end"
