491d01b9593ae2756132aa7a0b3739d05e7e3082
longzxr <longzxr@gmail.com>
2016-04-04 22:43:50 +0800

    update grade_examination

--- app/models/grade_examination.rb
+++ app/models/grade_examination.rb
@@ -1,2 +1,29 @@
+##!/usr/bin/ruby
+
+#require "rubygems"
+require "active_record"
+
 class GradeExamination < ActiveRecord::Base
+	self.table_name = 'xsjbxxb'
+
+	establish_connection(
+		:adapter => "oracle_enhanced", 
+		:host => "192.168.100.1",
+		:port => 1521,
+		:database => "orcl",
+		:username => "zfxfzb",
+		:password => "zfsoft_tzzy",
+	)
 end
+
+
+order = GradeExamination.find_by_sql("select * from xsjbxxb")
+
+first = order[0]
+p first.attributes
+
+#puts order
+#order.each do |k,v|
+#	puts value
+#end
+
